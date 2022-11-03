5a9a0a8ba8ffab2a9b9282baff17d7ddfd8bfc5d
Baklay Petro <pustota1988@gmail.com>
2022-11-03 12:07:20 +0200

    add-component-Dialogs

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import s from './Dialogs.modules.css';
+
+const Dialogs = () => {
+  return <div className={s.content}>Dialogs</div>;
+};
+
+export default Dialogs;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,10 +4,10 @@ import s from './Navbar.module.css';
 const Navbar = () => {
     return <nav className={s.nav}>
         <div className={s.item}>
-            <a>Profile</a>
+            <a href='#'>Profile</a>
         </div>
         <div className={`${s.item} ${s.active}`}>
-            <a>Messages</a>
+            <a href='#'>Messages</a>
         </div>
         <div className={s.item}>
             <a>News</a>
