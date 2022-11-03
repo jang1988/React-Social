0c3372798d8b4405210051d8074fa71e873aaed5
Baklay Petro <pustota1988@gmail.com>
2022-11-03 11:39:43 +0200

    start

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './Post.module.css';
+
+const Post = (props) => {
+
+  return (
+    <div className={s.item}>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt18RyNtXMld3_kycpSfltIKHMbWjkAF1OLQ&usqp=CAU' />
+        { props.message }
+          <div>
+        <span>like</span> { props.likesCount }
+      </div>
+    </div>
+  )
+}
+
+export default Post;
