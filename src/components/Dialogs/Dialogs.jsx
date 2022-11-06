0922522d379ddd15c4665c78189c2b6555b0636c
Baklay Petro <pustota1988@gmail.com>
2022-11-06 18:32:54 +0200

    map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,7 +7,7 @@ const DialogItem = ({ name, id }) => {
     <div className={s.dialog}>
       <NavLink
         className={({ isActive }) => (isActive ? s.active : '')}
-        to={"/dialogs/" + id}
+        to={'/dialogs/' + id}
       >
         {name}
       </NavLink>
@@ -15,28 +15,38 @@ const DialogItem = ({ name, id }) => {
   );
 };
 
-const Message = ({message}) => {
-  return (
-    <div className={s.dialog}>{message}</div>
-  )
-}
+const Message = ({ message }) => {
+  return <div className={s.dialog}>{message}</div>;
+};
 
 const Dialogs = () => {
+  let dialogs = [
+    { id: 1, name: 'Petro' },
+    { id: 2, name: 'Anna' },
+    { id: 3, name: 'Gleb' },
+    { id: 4, name: 'Nata' },
+    { id: 5, name: 'Illy' },
+    { id: 6, name: 'Max' },
+  ];
+
+  let messages = [
+    { id: 1, message: 'Hi!' },
+    { id: 2, message: 'How is you React' },
+    { id: 3, message: 'Yo' },
+  ];
+
+  let dialogsElements = dialogs.map((dialog) => (
+    <DialogItem key={dialog.id} name={dialog.name} id={dialog.id} />
+  ));
+
+  let messagesElements = messages.map((message) => (
+    <Message key={message.id} message={message.message} id={message.id} />
+  ));
+
   return (
     <div className={s.dialogs}>
-      <div className={s.dialogsItems}>
-        <DialogItem name="Petr" id="1" />
-        <DialogItem name="Anna" id="2" />
-        <DialogItem name="Gleb" id="3" />
-        <DialogItem name="Nata" id="4" />
-        <DialogItem name="Illy" id="5" />
-        <DialogItem name="Max" id="6" />
-      </div>
-      <div className={s.messages}>
-        <Message message="Hi!"/>
-        <Message message="How is you React"/>
-        <Message message="Yo"/>
-      </div>
+      <div className={s.dialogsItems}>{dialogsElements}</div>
+      <div className={s.messages}>{messagesElements}</div>
     </div>
   );
 };
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,21 +3,26 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
+  let posts = [
+    { id: 1, message: 'Hi, how are you?', likesCount: 0 },
+    { id: 2, message: "It's my first post", likesCount: 23 },
+  ];
+
+  let postsElements = posts.map((post) => (
+    <Post key={post.id} message={post.message} likesCount={post.likesCount} />
+  ));
   return (
     <div className={s.postBlock}>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea className={s.textarea}></textarea>
         </div>
         <div>
-          <button>Add post</button>
+          <button className={s.btn}>Add post</button>
         </div>
       </div>
-      <div className={s.posts}>
-        <Post message="Hi, how are you?" likesCount="0" />
-        <Post message="It's my first post" likesCount="23" />
-      </div>
+      <div className={s.posts}>{postsElements}</div>
     </div>
   );
 };
